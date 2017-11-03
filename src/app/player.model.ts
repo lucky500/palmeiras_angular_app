73b3761cb7d1ebf4273d081d8989ec9b9bbd5710
Luciano Oliveira <luciano101@msn.com>
2017-11-03 11:41:39 -0700

    add player model

---
+++ src/app/player.model.ts
@@ -0,0 +1,5 @@
+export class Player {
+  constructor(public img: string, public name: string, public age: string, public position: string, public numberOfGames: string, public description: string){
+
+  }
+}
